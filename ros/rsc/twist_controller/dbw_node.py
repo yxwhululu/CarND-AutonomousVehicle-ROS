ce3a50747b552681ebdc8704315ee7e2618bb0ac
YeXinwei <47932914+yxwhululu@users.noreply.github.com>
2019-03-02 20:15:58 +0800

    Update dbw_node.py

--- ros/rsc/twist_controller/dbw_node.py
+++ ros/rsc/twist_controller/dbw_node.py
@@ -65,10 +65,10 @@ class DBWNode(object):
         rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb)
         rospy.Subscriber('/current_velocity',TwistStamped, self.velocity_cb)
         
-        self.current_vel = None
+        self.current_vel = None  
         self.curr_ang_vel = None
         self.dbw_enabled = None
-        self.linear_vel = None
+        self.linear_vel = None  #the target velocity?
         self.angular_vel = None
         self.throttle = self.steering = self.brake = 0
 
